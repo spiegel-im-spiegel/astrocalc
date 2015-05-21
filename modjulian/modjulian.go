4ae0ff2bc401983216227d0b39068225de96abdc
Der Spiegel im Spiegel <spiegel-im-spiegel@users.noreply.github.com>
2015-05-21 16:57:25 +0900

    Create modjulian.go

---
+++ modjulian/modjulian.go
@@ -0,0 +1,32 @@
+package modjulian
+
+import "time"
+
+func DayNumber(t time.Time) int64 {
+	if t.Sub(time.Unix(0, 0)) >= 0 {
+		return dnUnix(t)
+	} else {
+		return dnGregorian(t)
+	}
+}
+
+func dnGregorian(t time.Time) int64 {
+	y := int64(t.Year())
+	m := int64(t.Month())
+	if m < 3 {
+		y -= 1
+		m += 9
+	} else {
+		m -= 3
+	}
+	d := int64(t.Day()) - 1
+	return (1461*y)/4 + y/400 - y/100 + (153*m+2)/5 + d - 678881
+}
+
+func dnUnix(t time.Time) int64 {
+	const (
+		onday   = int64(86400) //seconds
+		baseDay = int64(40587) //Modified Julian Date at January 1, 1970
+	)
+	return (t.Unix())/onday + baseDay
+}
