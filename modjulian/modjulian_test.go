990ab3176cf8642ceae78569494c25b010a92696
Der Spiegel im Spiegel <spiegel-im-spiegel@users.noreply.github.com>
2015-05-22 16:42:54 +0900

    Create modjulian_test.go

---
+++ modjulian/modjulian_test.go
@@ -0,0 +1,38 @@
+package modjulian
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+type mjdnTest struct { //test case for DayNumber
+	in time.Time //input data
+	out int64 //expected result
+}
+
+var mjdnTests []mjdnTest  //test cases for DayNumber
+
+func TestMain(m *testing.M) {
+	//initialization
+ 	mjdnTests = []mjdnTest {  //test cases for DayNumber
+		{time.Date(1969, 12, 31, 0, 0, 0, 0, time.UTC), int64(40586)},
+		{time.Date(1970,  1,  1, 0, 0, 0, 0, time.UTC), int64(40587)},
+		{time.Date(2015,  1,  1, 0, 0, 0, 0, time.UTC), int64(57023)},
+	}
+
+	//start test
+    code := m.Run()
+
+	//termination
+    os.Exit(code)
+}
+
+func TestModifiedJulianDayNumber(t *testing.T) {
+	for _, testCase := range mjdnTests {
+		result := DayNumber(testCase.in)
+		if result != testCase.out {
+			t.Errorf("DayNumber of \"%v\" = %d, want %d.", testCase.in, result, testCase.out)
+		}
+	}
+}
